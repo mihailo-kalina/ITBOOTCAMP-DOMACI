fbf0bcd4d2d1f01537dcc38b037942c734d23bc3
Mihailo <mihailo.v89@hotmail.com>
2021-05-25 12:54:46 +0200

    vracam boje u crno

--- Nedelja_6/Sreda/domac2ponedeljak.php
+++ Nedelja_6/Sreda/domac2ponedeljak.php
@@ -1,7 +1,3 @@
-Dat je slucajan broj od 1 do 64.
-Napraviti tabelu 8x8. Sirina i visina svake celije je 30px i imaju border 1px. 
-Celiji sa datim brojem postaviti crnu pozadinu. (preko klase ili inline style). 
-Npr ako je broj 11, onda je celija u drugom redu i 3.koloni obojena crno.
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,16 +20,13 @@ Npr ako je broj 11, onda je celija u drugom redu i 3.koloni obojena crno.
 <body>
 <?php
     $a = mt_rand(1,64);
-    $b = mt_rand(0,255);
-    $c = mt_rand(0,255);
-    $d = mt_rand(0,255);
-
+   
     echo "<table>";
     for($i = 0; $i <= 7; $i++){
         echo "<tr>";
         for($j=1; $j<=8; $j++){
             if($a == ($i*8+$j)){
-                echo "<td style='background:rgb($b,$c,$d);color:white'>".($i*8+$j)."</td>";
+                echo "<td style='background:black;color:white'>".($i*8+$j)."</td>";
             }else{
                 echo "<td></td>";
             }
